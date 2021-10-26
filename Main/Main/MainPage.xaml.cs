bddf8baecbc91b4c80c5b107a30b7f81b55062b6
Troy <shutman18@gmail.com>
2021-10-27 10:20:33 +1300

    Design Tabbed UI

---
+++ Main/Main/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Main
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void espresso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EspressoList());
+        }
+
+        private async void hotDrinks_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new hotDrinksList());
+        }
+
+        private async void coldDrinks_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new coldDrinksList());
+        }
+
+        private async void quickBites_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new quickBitesList());
+        }
+    }
+}
---
+++ Main/Main/MainTabbedPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Main
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void espresso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EspressoList());
+        }
+
+        private async void hotDrinks_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new hotDrinksList());
+        }
+
+        private async void coldDrinks_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new coldDrinksList());
+        }
+
+        private async void quickBites_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new quickBitesList());
+        }
+    }
+}
